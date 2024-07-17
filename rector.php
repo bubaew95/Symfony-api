0bde7f94bbf7efa065700ccc37b4d188c0dd33ab
borz <fank-1191@mail.ru>
2024-07-18 00:39:42 +0300

    formating code

--- rector.php
+++ rector.php
@@ -7,22 +7,22 @@ use Rector\Symfony\Set\SymfonySetList;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/src',
+        __DIR__.'/config',
+        __DIR__.'/public',
+        __DIR__.'/src',
     ])
-    ->withSymfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml')
+    ->withSymfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml')
     ->withSets([
         SymfonySetList::SYMFONY_62,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ])
     ->withSkip([
-        __DIR__ . '/.git',
-        __DIR__ . '/node_modules',
-        __DIR__ . '/vendor',
-        __DIR__ . '/src/Kernel.php',
-        __DIR__ . '/src/Migrations',
+        __DIR__.'/.git',
+        __DIR__.'/node_modules',
+        __DIR__.'/vendor',
+        __DIR__.'/src/Kernel.php',
+        __DIR__.'/src/Migrations',
     ])
     ->withPhpSets(php82: true)
     ->withPreparedSets(
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,12 +6,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class BookController extends AbstractController
 {
-    /**
-     * @Route('/index', name="test_app")
-     */
+    #[Route('/', name: 'app_main' )]
     public function index(): Response
     {
         return new Response('test');
