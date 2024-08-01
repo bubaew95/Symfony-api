84563f1930fa47640ffc1818596e30ee58e9f594
borz <fank-1191@mail.ru>
2024-08-01 20:59:04 +0300

    Авто рефакторинг кода

--- src/Normalizer/AddUserGroupsNormalizer.php
+++ src/Normalizer/AddUserGroupsNormalizer.php
@@ -10,13 +10,13 @@ use Symfony\Component\Serializer\SerializerInterface;
 #[AsDecorator('api_platform.jsonld.normalizer.item')]
 class AddUserGroupsNormalizer implements NormalizerInterface, SerializerAwareInterface
 {
-    public function __construct(private NormalizerInterface $normalizer)
+    public function __construct(private readonly NormalizerInterface $normalizer)
     {
     }
 
     public function normalize(mixed $object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
     {
-//        dump('IT WORKS');
+        //        dump('IT WORKS');
 
         return $this->normalizer->normalize($object, $format, $context);
     }
